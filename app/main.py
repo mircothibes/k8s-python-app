f4a749e8a9b014de41eb011e859ffb6552d96295
Marcos Vinicius Thibes Kemer <mircothibes@gmail.com>
2025-09-30 18:04:19 +0200

    main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
-
-@app.get("/healthz")
-def healthz():
-    return {"status": "ok"}
-
-@app.get("/readyz")
-def readyz():
-    return {"ready": True}
 
 @app.get("/")
 def root():
     return {"message": "Hello, Kubernetes!"}
+
+@app.get("/healthz")
+def healthz():
+    return JSONResponse(content={"status": "ok"})
+
+@app.get("/readyz")
+def readyz():
+    return {"ready": True}
